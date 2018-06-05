b3e2d164e22997ad1d51d439e43e60a584d03129
tapoo tapoo <laurendeau.elliot@gmail.com>
2018-06-04 23:59:01 -0400

    Sensor will print UV level every second

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-"# LCD-veml6070-uv-light-sensor" 
+# Displays UV level on a LCD sensor shield
+
+This project is used to calibrate a Duplicator 7 DLP printer.
+
+3d printed grid:
+https://www.thingiverse.com/thing:2813241
+
+UV sensor
+https://learn.adafruit.com/adafruit-veml6070-uv-light-sensor-breakout/overview
+
+LCD shield
+https://learn.adafruit.com/rgb-lcd-shield
+
+Wiring:
+https://cdn-learn.adafruit.com/assets/assets/000/032/466/medium800/force___flex_wiring.jpg
+https://i.imgur.com/aEgHt9D.png
+
+Final wires:
+https://cdn.discordapp.com/attachments/265638712623235092/453404825141641216/20180604_234743.jpg
+https://cdn.discordapp.com/attachments/265638712623235092/453404824512233472/20180604_234724.jpg
+https://cdn.discordapp.com/attachments/265638712623235092/453404823807721482/20180604_234734.jpg
--- uv-sensor-sketch/uv-sensor-sketch.ino
+++ uv-sensor-sketch/uv-sensor-sketch.ino
@@ -1,17 +1,57 @@
-#include <Wire.h>
-#include "Adafruit_VEML6070.h"
+/*********************
 
+Example code for the Adafruit RGB Character LCD Shield and Library
+
+This code displays text on the shield, and also reads the buttons on the keypad.
+When a button is pressed, the backlight changes color.
+
+**********************/
+
+// include the library code:
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h>
+#include <Adafruit_VEML6070.h>
+#include <utility/Adafruit_MCP23017.h>
+
+
+// The shield uses the I2C SCL and SDA pins. On classic Arduinos
+// this is Analog 4 and 5 so you can't use those for analogRead() anymore
+// However, you can connect other I2C sensors to the I2C bus and share
+// the I2C bus.
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 Adafruit_VEML6070 uv = Adafruit_VEML6070();
 
+// These #defines make it easy to set the backlight color
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
 void setup() {
+  // Debugging output
   Serial.begin(9600);
-  Serial.println("VEML6070 sensor init");
-  uv.begin(VEML6070_1_T);  // pass in the integration time constant
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+
+  // Print a message to the LCD. We track how long it takes since
+  // this library has been optimized a bit and we're proud of it :)
+
+  
+  uv.begin(VEML6070_1_T);
+  lcd.setBacklight(WHITE);
 }
 
-
+uint8_t i=0;
 void loop() {
-  Serial.print("UV light level: "); Serial.println(uv.readUV());
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("UV Light level:");
   
+  lcd.setCursor(0,1);
+  lcd.print(uv.readUV());
   delay(1000);
+  
 }
